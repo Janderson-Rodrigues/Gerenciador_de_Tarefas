794a5dcca7f1e2ba5777160acc1b8bea4f71b2af
Janderson <jandersonrodrigues216@gmail.com>
2025-09-26 10:40:31 -0300

    Controller add config

--- src/main/java/com/jandersonrodrigues/todolist/controller/TodoController.java
+++ src/main/java/com/jandersonrodrigues/todolist/controller/TodoController.java
@@ -1,4 +1,37 @@
 package com.jandersonrodrigues.todolist.controller;
 
+import com.jandersonrodrigues.todolist.entity.Todo;
+import com.jandersonrodrigues.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
 public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+     return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable Long id) {
+        return todoService.delete(id);
+    }
 }
