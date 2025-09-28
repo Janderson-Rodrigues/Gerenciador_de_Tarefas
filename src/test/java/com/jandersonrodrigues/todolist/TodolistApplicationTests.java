bc23834ccf080b43c22ae2533c16784a0ceda659
Janderson <jandersonrodrigues216@gmail.com>
2025-09-28 19:29:54 -0300

    test version 0.1

--- src/test/java/com/jandersonrodrigues/todolist/TodolistApplicationTests.java
+++ src/test/java/com/jandersonrodrigues/todolist/TodolistApplicationTests.java
@@ -1,13 +1,22 @@
 package com.jandersonrodrigues.todolist;
 
+import com.jandersonrodrigues.todolist.entity.Todo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest
 class TodolistApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreatTodoSuccess() {
+		var todo = new Todo("todo 1", "desc todo", false, 1);
 	}
 
+	@Test
+	void testCreatTodoFailure() {
+	}
 }
